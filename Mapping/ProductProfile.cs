67e036ee9781d677686e959d5da514847bea1b42
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2024-09-11 08:47:53 +0200

    refactoring

--- Mapping/ProductProfile.cs
+++ Mapping/ProductProfile.cs
@@ -9,7 +9,7 @@ namespace AuthJWTAPI.Mapping
         public ProductProfile()
         {
             CreateMap<ProductDTO, Product>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())  // Ignorowanie Id podczas tworzenia nowego produktu
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
 
